0c29ca36978171bbc123fda5d914919776f8077e
Artem Solopiy <artem.solopiy.gdc@ts.fujitsu.com>
2019-12-05 23:40:25 +0300

    Implemented CUD for controller

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using StarChart.Data;
+using StarChart.Models;
 
 namespace StarChart.Controllers
 {
@@ -55,6 +56,62 @@ namespace StarChart.Controllers
                 item.Satellites = _context.CelestialObjects.Where(e => e.OrbitedObjectId == item.Id).ToList();
             }
             return Ok(items);
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody]CelestialObject model)
+        {
+            _context.CelestialObjects.Add(model);
+            _context.SaveChanges();
+            return CreatedAtRoute("GetById", new {id = model.Id}, model);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, [FromBody]CelestialObject model)
+        {
+            var item = _context.CelestialObjects.FirstOrDefault(e => e.Id == id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+
+            item.Name = model.Name;
+            item.OrbitedObjectId = model.OrbitedObjectId;
+            item.OrbitalPeriod = model.OrbitalPeriod;
+
+            _context.CelestialObjects.Update(item);
+            _context.SaveChanges();
+            return NoContent();
+        }
+
+        [HttpPatch("{id}/{name}")]
+        public IActionResult RenameObject(int id, string name)
+        {
+            var item = _context.CelestialObjects.FirstOrDefault(e => e.Id == id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+
+            item.Name = name;
+
+            _context.CelestialObjects.Update(item);
+            _context.SaveChanges();
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var items = _context.CelestialObjects.Where(e => e.Id == id || e.OrbitedObjectId == id).ToList();
+            if (!items.Any())
+            {
+                return NotFound();
+            }
+
+            _context.RemoveRange(items);
+            _context.SaveChanges();
+            return NoContent();
         }
     }
 }
