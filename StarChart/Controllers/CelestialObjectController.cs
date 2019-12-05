6d5f741351968e40f928c6e477543d8ee34796dc
Artem Solopiy <artem.solopiy.gdc@ts.fujitsu.com>
2019-12-05 22:10:38 +0300

    Implemented controller

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
 using StarChart.Data;
 
 namespace StarChart.Controllers
@@ -12,6 +13,45 @@ namespace StarChart.Controllers
         public CelestialObjectController(ApplicationDbContext context)
         {
             _context = context;
+        }
+
+        [HttpGet("{id:int}")]
+        public IActionResult GetById(int id)
+        {
+            var item = _context.CelestialObjects.FirstOrDefault(e => e.Id == id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+
+            item.Satellites = _context.CelestialObjects.Where(e => e.OrbitedObjectId == item.Id).ToList();
+
+            return Ok(item);
+        }
+
+        [HttpGet("{name}")]
+        public IActionResult GetByName(string name)
+        {
+            var item = _context.CelestialObjects.FirstOrDefault(e => e.Name == name);
+            if (item == null)
+            {
+                return NotFound();
+            }
+
+            item.Satellites = _context.CelestialObjects.Where(e => e.OrbitedObjectId == item.Id).ToList();
+
+            return Ok(item);
+        }
+
+        [HttpGet()]
+        public IActionResult GetResult()
+        {
+            var items = _context.CelestialObjects.ToList();
+            foreach (var item in items)
+            {
+                item.Satellites = items;
+            }
+            return Ok(items);
         }
     }
 }
